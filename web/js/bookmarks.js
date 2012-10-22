94c84c4521e4f4f01fc1be22a5523ab75736dea3
Benjamin Eberlei <kontakt@beberlei.de>
2012-10-22 16:31:32 +0200

    fix bug

--- web/js/bookmarks.js
+++ web/js/bookmarks.js
@@ -129,7 +129,7 @@ var BookmarkApplication = Backbone.View.extend({
             dataType: 'json',
             success: _.bind(this.showBookmarkDetails, this),
             error: function(data) {
-                console.log(data);
+                alert(data.error);
             }
         });
         return false;
