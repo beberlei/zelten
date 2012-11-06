5f56a00d2906d5e2a7343753fa1f81cd2294e3dd
Benjamin Eberlei <kontakt@beberlei.de>
2012-11-06 23:21:50 +0100

    Fix UrlLinker using another regexp from ezcDocument and rewrite some logic

--- src/Kwi/UrlLinker.php
+++ src/Kwi/UrlLinker.php
@@ -14,6 +14,32 @@ namespace Kwi;
 
 class UrlLinker
 {
+    /**
+     * PCRE regular expression for detection of URLs in texts.
+     */
+    const REGEXP_INLINE_LINK = '(
+        (?:^|[\s,.!?])
+            (?# Ignore matching braces around the URL)
+                (<)?
+                    (\[)?
+                        (\()?
+                            (?# Ignore quoting around the URL)
+                            ([\'"]?)
+                                (?# Actually match the URL)
+                                (?P<match>
+                                    (?P<url>[a-z]+://[^\s]*?) |
+                                    (?:mailto:)?(?P<mail>[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})
+                                )
+                            \4
+                        (?(3)\))
+                    (?(2)\])
+                (?(1)>)
+            (?# Ignore common punctuation after the URL)
+        [.,?!]?(?:\s|$)
+    )Sxm';
+
+    const TRAILING_PUNCTUATION = ")'?.!,;:";
+
     private $rexUrlLinker;
     /**
      *  $validTlds is an associative array mapping valid TLDs to the value true.
@@ -30,21 +56,6 @@ class UrlLinker
      */
     public function __construct()
     {
-        $rexScheme    = 'https?://';
-        // $rexScheme    = "$rexScheme|ftp://"; // Uncomment this line to allow FTP addresses.
-        $rexDomain    = '(?:[-a-zA-Z0-9]{1,63}\.)+[a-zA-Z][-a-zA-Z0-9]{1,62}';
-        $rexIp        = '(?:[1-9][0-9]{0,2}\.|0\.){3}(?:[1-9][0-9]{0,2}|0)';
-        $rexPort      = '(:[0-9]{1,5})?';
-        $rexPath      = '(/[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]*?)?';
-        $rexQuery     = '(\?[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
-        $rexFragment  = '(#[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
-        $rexUsername  = '[^]\\\\\x00-\x20\"(),:-<>[\x7f-\xff]{1,64}';
-        $rexPassword  = $rexUsername; // allow the same characters as in the username
-        $rexUrl       = "($rexScheme)?(?:($rexUsername)(:$rexPassword)?@)?($rexDomain|$rexIp)($rexPort$rexPath$rexQuery$rexFragment)";
-        $rexTrailPunct= "[)'?.!,;:]"; // valid URL characters which are not part of the URL if they appear at the very end
-        $rexNonUrl    = "[^-_$+.!*'(),;/?:@=&a-zA-Z0-9]"; // characters that should never appear in a URL
-
-        $this->rexUrlLinker = "{\\b$rexUrl(?=$rexTrailPunct*($rexNonUrl|$))}";
         $this->validTlds = array_fill_keys(explode(" ", ".ac .ad .ae .aero .af .ag .ai .al .am .an .ao .aq .ar .arpa .as .asia .at .au .aw .ax .az .ba .bb .bd .be .bf .bg .bh .bi .biz .bj .bm .bn .bo .br .bs .bt .bv .bw .by .bz .ca .cat .cc .cd .cf .cg .ch .ci .ck .cl .cm .cn .co .com .coop .cr .cu .cv .cw .cx .cy .cz .de .dj .dk .dm .do .dz .ec .edu .ee .eg .er .es .et .eu .fi .fj .fk .fm .fo .fr .ga .gb .gd .ge .gf .gg .gh .gi .gl .gm .gn .gov .gp .gq .gr .gs .gt .gu .gw .gy .hk .hm .hn .hr .ht .hu .id .ie .il .im .in .info .int .io .iq .ir .is .it .je .jm .jo .jobs .jp .ke .kg .kh .ki .km .kn .kp .kr .kw .ky .kz .la .lb .lc .li .lk .lr .ls .lt .lu .lv .ly .ma .mc .md .me .mg .mh .mil .mk .ml .mm .mn .mo .mobi .mp .mq .mr .ms .mt .mu .museum .mv .mw .mx .my .mz .na .name .nc .ne .net .nf .ng .ni .nl .no .np .nr .nu .nz .om .org .pa .pe .pf .pg .ph .pk .pl .pm .pn .post .pr .pro .ps .pt .pw .py .qa .re .ro .rs .ru .rw .sa .sb .sc .sd .se .sg .sh .si .sj .sk .sl .sm .sn .so .sr .st .su .sv .sx .sy .sz .tc .td .tel .tf .tg .th .tj .tk .tl .tm .tn .to .tp .tr .travel .tt .tv .tw .tz .ua .ug .uk .us .uy .uz .va .vc .ve .vg .vi .vn .vu .wf .ws .xn--0zwm56d .xn--11b5bs3a9aj6g .xn--3e0b707e .xn--45brj9c .xn--80akhbyknj4f .xn--80ao21a .xn--90a3ac .xn--9t4b11yi5a .xn--clchc0ea0b2g2a9gcd .xn--deba0ad .xn--fiqs8s .xn--fiqz9s .xn--fpcrj9c3d .xn--fzc2c9e2c .xn--g6w251d .xn--gecrj9c .xn--h2brj9c .xn--hgbk6aj7f53bba .xn--hlcj6aya9esc7a .xn--j6w193g .xn--jxalpdlp .xn--kgbechtv .xn--kprw13d .xn--kpry57d .xn--lgbbat1ad8j .xn--mgb9awbf .xn--mgbaam7a8h .xn--mgbayh7gpa .xn--mgbbh1a71e .xn--mgbc0a9azcg .xn--mgberp4a5d4ar .xn--o3cw4h .xn--ogbpf8fl .xn--p1ai .xn--pgbs0dh .xn--s9brj9c .xn--wgbh1c .xn--wgbl6a .xn--xkc2al3hye2a .xn--xkc2dl3a5ee0h .xn--yfro4i67o .xn--ygbi2ammx .xn--zckzah .xxx .ye .yt .za .zm .zw"), true);
     }
 
@@ -57,8 +68,10 @@ class UrlLinker
         $html = '';
 
         $position = 0;
-        while (preg_match($this->rexUrlLinker, $text, $match, PREG_OFFSET_CAPTURE, $position)) {
+        while (preg_match(self::REGEXP_INLINE_LINK, $text, $match, PREG_OFFSET_CAPTURE, $position)) {
             list($url, $urlPosition) = $match[0];
+
+            $url = rtrim($url, self::TRAILING_PUNCTUATION);
 
             // Add the text leading up to the URL.
             $html .= htmlspecialchars(substr($text, $position, $urlPosition - $position));
@@ -70,13 +83,23 @@ class UrlLinker
                 continue;
             }
 
-            $scheme      = $match[1][0];
-            $username    = $match[2][0];
-            $password    = $match[3][0];
-            $domain      = $match[4][0];
-            $afterDomain = $match[5][0]; // everything following the domain
-            $port        = $match[6][0];
-            $path        = $match[7][0];
+            $urlParts = array_merge(array(
+                'scheme' => '',
+                'username' => '',
+                'password' => '',
+                'port' => '',
+                'path' => '',
+                'query' => '',
+                'fragment' => '',
+                ), parse_url($url));
+
+            $scheme      = $urlParts['scheme'];
+            $username    = $urlParts['username'];
+            $password    = $urlParts['password'];
+            $domain      = $urlParts['host'];
+            $afterDomain = $urlParts['path'] . $urlParts['query'] . $urlParts['fragment']; // everything following the domain
+            $port        = $urlParts['port'];
+            $path        = $urlParts['path'];
 
             // Check that the TLD is valid or that $domain is an IP address.
             $tld = strtolower(strrchr($domain, '.'));
--- tests/Zelten/Tests/Stream/MessageTest.php
+++ tests/Zelten/Tests/Stream/MessageTest.php
@@ -7,14 +7,30 @@ use Kwi\UrlLinker;
 
 class MessageTest extends TestCase
 {
+    static public function dataUrlLinker()
+    {
+        return array(
+            array(
+                'http://f.cl.ly/items/0S142I071o130u1h2145/Screen%20Shot%202012-11-01%20at%208.08.25%20PM.png',
+                '<a href="http://f.cl.ly/items/0S142I071o130u1h2145/Screen%20Shot%202012-11-01%20at%208.08.25%20PM.png">f.cl.ly/items/0S142I071o130u1h2145/Screen%20Shot%202012-11-01%20at%208.08.25%20PM.png</a>'
+            ),
+            array(
+                'https://foo:bar@domain.com/lol%20catz!',
+                '<a href="https://foo:bar&#64;domain.com/lol%20catz">domain.com/lol%20catz</a>!',
+            ),
+        );
+    }
+
     /**
      * @group GH-62
+     * @dataProvider dataUrlLinker
      */
-    public function testUrlLinker()
+    public function testUrlLinker($url, $expected)
     {
         $linker = new UrlLinker();
-        $response = $linker->parse('http://f.cl.ly/items/0S142I071o130u1h2145/Screen%20Shot%202012-11-01%20at%208.08.25%20PM.png');
-        var_dump($response);
+        $response = $linker->parse($url);
+
+        $this->assertEquals($expected, $response);
     }
 }
 
