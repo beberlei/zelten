f0bf70b1c9ee4a8cbc8f0b1fbd5e84404f6e2a2a
Benjamin Eberlei <kontakt@beberlei.de>
2012-11-08 18:58:59 +0100

    Fix various url linker bugs, closes GH-128

--- tests/Zelten/Tests/Stream/MessageTest.php
+++ tests/Zelten/Tests/Stream/MessageTest.php
@@ -21,6 +21,22 @@ class MessageTest extends TestCase
             array(
                 "testing short link first in line:\nhttp://foo.com",
                 "testing short link first in line:\n<a href=\"http://foo.com\">foo.com</a>",
+            ),
+            array(
+                "Still cant login on Zelten: http://poweruser.tent.is/posts/zwyyu3.\nWhere is the problem?",
+                "Still cant login on Zelten: <a href=\"http://poweruser.tent.is/posts/zwyyu3\">poweruser.tent.is/posts/zwyyu3</a>.\nWhere is the problem?"
+            ),
+            array(
+                "Bug: https://github.com/tent/tentd/issues/42. It's preve",
+                'Bug: <a href="https://github.com/tent/tentd/issues/42">github.com/tent/tentd/issues/42</a>. It\'s preve',
+            ),
+            array(
+                "..purposes, like http://gaming.bitnbang.com and http://design.bitnbang.com",
+                '..purposes, like <a href="http://gaming.bitnbang.com">gaming.bitnbang.com</a> and <a href="http://design.bitnbang.com">design.bitnbang.com</a>'
+            ),
+            array(
+                "See this thread (https://github.com/beberlei/zelten/issues/106).",
+                'See this thread (<a href="https://github.com/beberlei/zelten/issues/106">github.com/beberlei/zelten/issues/106</a>).'
             )
         );
     }
