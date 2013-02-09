499c7de2966b8d26fcc1f23892c35edd719b6827
Benjamin Eberlei <kontakt@beberlei.de>
2013-02-09 15:01:45 +0100

    Merge GuzzleBundle Toolbar+DataCollector into Zelten for debugging in dev mode

--- config/dev.php
+++ config/dev.php
@@ -3,6 +3,8 @@
 use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
 
+use Zelten\Util\Profiler\GuzzleDataCollector;
+
 // include the prod configuration
 require __DIR__.'/prod.php';
 
@@ -17,3 +19,22 @@ $app->register($p = new WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__.'/../cache/profiler',
     ));
 $app->mount('/_profiler', $p);
+
+$templates = $app['data_collector.templates'];
+$templates[] = array('guzzle', 'Toolbar/guzzle.html.twig');
+$app['data_collector.templates'] = $templates;
+
+$collectors = $app['data_collectors'];
+$collectors['guzzle'] = $app->share(function ($app) {
+    return new GuzzleDataCollector($app['tent.client.history']);
+});
+$app['data_collectors'] = $collectors;
+
+$app['tent.client.history'] = $app->share(function ($app) {
+    return new \Guzzle\Plugin\History\HistoryPlugin;
+});
+$app['tent.http.client'] = $app->share($app->extend('tent.http.client', function($client, $app) {
+    $client->addSubscriber($app['tent.client.history']);
+
+    return $client;
+}));
---
+++ src/Zelten/Util/Profiler/GuzzleDataCollector.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Zelten\Util\Profiler;
+
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Guzzle\Plugin\History\HistoryPlugin;
+use Guzzle\Http\Message\RequestInterface;
+use Guzzle\Http\Message\Response as GuzzleResponse;
+
+/**
+ * GuzzleDataCollector.
+ *
+ * @author Ludovic Fleury <ludo.flery@gmail.com>
+ */
+class GuzzleDataCollector extends DataCollector
+{
+    private $profiler;
+
+    public function __construct(HistoryPlugin $profiler)
+    {
+        $this->profiler = $profiler;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $this->data = array(
+            'calls'       => array(),
+            'error_count' => 0,
+            'methods'     => array(),
+            'total_time'  => 0,
+        );
+
+        foreach ($this->profiler as $call) {
+            $error = false;
+            $request = $call;
+            $response = $request->getResponse();
+
+            $requestContent = null;
+            $responseContent = $response->getBody(true);
+
+            $time = array(
+                'total' => $response->getInfo('total_time'),
+                'connection' => $response->getInfo('connect_time')
+            );
+
+            $this->data['total_time'] += $response->getInfo('total_time');
+
+            if (!isset($this->data['methods'][$request->getMethod()])) {
+                $this->data['methods'][$request->getMethod()] = 0;
+            }
+
+            $this->data['methods'][$request->getMethod()]++;
+
+            if ($response->isError()) {
+                $this->data['error_count']++;
+                $error = true;
+            }
+
+            $this->data['calls'][] = array(
+                'request' => $this->sanitizeRequest($request),
+                'requestContent' => $requestContent,
+                'response' => $this->sanitizeResponse($response),
+                'responseContent' => $responseContent,
+                'time' => $time,
+                'error' => $error
+            );
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getCalls()
+    {
+        return isset($this->data['calls']) ? $this->data['calls'] : array();
+    }
+
+    /**
+     * @return int
+     */
+    public function countErrors()
+    {
+        return isset($this->data['error_count']) ? $this->data['error_count'] : 0;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMethods()
+    {
+        return isset($this->data['methods']) ? $this->data['methods'] : array();
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalTime()
+    {
+        return isset($this->data['total_time']) ? $this->data['total_time'] : 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'guzzle';
+    }
+
+    /**
+     * @param RequestInterface $request
+     *
+     * @return array
+     */
+    private function sanitizeRequest(RequestInterface $request)
+    {
+        return array(
+            'method'           => $request->getMethod(),
+            'path'             => $request->getPath(),
+            'scheme'           => $request->getScheme(),
+            'host'             => $request->getHost(),
+            'query'            => $request->getQuery(),
+            'headers'          => $request->getHeaders(),
+            'query_parameters' => $request->getUrl(true)->getQuery(),
+        );
+    }
+
+    /**
+     * @param GuzzleResponse $response
+     *
+     * @return array
+     */
+    private function sanitizeResponse(GuzzleResponse $response)
+    {
+        return array(
+            'statusCode'   => $response->getStatusCode(),
+            'reasonPhrase' => $response->getReasonPhrase(),
+            'headers'      => $response->getHeaders(),
+        );
+    }
+}
